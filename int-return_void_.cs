2efe2e59d01092019843f8d34474607085ebfac8
Kate <tsvetkova.e.i@mail.ru>
2022-12-03 00:11:54 +0300

    diferentfunction

--- int-return_void_.cs
+++ int-return_void_.cs
@@ -1,4 +1,5 @@
-﻿void FillArray (int [] collection)
+﻿void FillArray (int[] collection)
+// заполнили рандомными числами индексы от 0 до 10
 {
     int length = collection.Length;
     int index =0;
@@ -12,16 +13,18 @@
 }
 
 void PrintArray(int [] col)
+// Зафиксировали индексы и запомнили за ними числа массива
 {
     int count= col.Length;
     int position=0;
     while (position<count)
     {
-        Console.WriteLine (col[position]);
+        Console.WriteLine ($"col[{position}] = {col[position]}");
         position++;
     }
 }
 int IndexOf(int[]collection,int find)
+// Запомнили первый индекс искомого числа
 {
     int count =collection.Length;
     int index =0;
@@ -37,13 +40,17 @@ int IndexOf(int[]collection,int find)
     }
     return position;
 }
-int [] array = new int [10];
-FillArray(array);
-array[4] = 4;
-array [6] = 4;
 
+int [] array = new int [10];
+
+FillArray(array);
+//array[4] = 4;
+//array [6] = 4;
+
+Console.WriteLine("My array");
 PrintArray(array);
 Console.WriteLine();
 
-int pos = IndexOf(array,444);
-Console.WriteLine(pos);
+int value = 4;
+int pos = IndexOf(array,value);
+Console.WriteLine($"pos: {pos} for value: {value}");
